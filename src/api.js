eaf6a1968ff95a6148f6fb558994bc6c2ae6b63b
vishal demo <vishalkanrar.1708@gmail.com>
2024-01-18 20:17:59 +0530

    Weather application completed

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '62f2340a86mshf6c29e6509993fcp1638fdjsn0e864dd9e01a',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1b288bc8484490d66a14fb1efeb08fff";
